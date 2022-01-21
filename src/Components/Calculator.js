a18cf2278f105dc9b7e11a72978a066d020bb4e8
Roberto Vladescu <60289462+robertovladescu@users.noreply.github.com>
2022-01-21 19:24:17 +0100

    Creating the components

---
+++ src/Components/Calculator.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import Button from './Button';
+import Keypad from './Keypad';
+import '../ComponentStyle/Calculator.css';
+import Display from './Display';
+
+class Calculator extends Component {
+    render(){
+        return(
+            <div className="Calculator">
+                <Display data={this.state.data}/>
+                <Keypad>
+                    <Button label="C" value="clear" />
+                    <Button label="7" value="7" />
+                    <Button label="4" value="4" />
+                    <Button label="1" value="1" />
+                    <Button label="0" value="0" />
+
+                    <Button label="/" value="/" />
+                    <Button label="8" value="8" />
+                    <Button label="5" value="5" />
+                    <Button label="2" value="2" />
+                    <Button label="." value="." />
+
+                    <Button label="x" value="*" />
+                    <Button label="9" value="9" />
+                    <Button label="6" value="6" />
+                    <Button label="3" value="3" />
+                    <Button label="" value="null" />
+
+                    <Button label="-" value="-" />
+                    <Button label="+" size="2" value="+" />
+                    <Button label="=" size="2" value="equal" />
+                </Keypad>
+            </div>
+        );
+    }
+}
+
+export default Calculator;
